3b5e0540b5a806a81fce40e9625c6503b4121044
Vrushal123456 <sagane.vrushal@gmail.com>
2025-09-17 01:27:43 +0530

    third commit

--- tests/BasicInterview/dropdown.spec.ts
+++ tests/BasicInterview/dropdown.spec.ts
@@ -13,3 +13,17 @@ await page.locator("#twotabsearchtextbox").fill("macbook");
 
 
 })
+
+test.only('dynamic dropdown scenario',async({page})=>{
+
+
+await page.goto("https://www.amazon.in/stores/HomePage/page/C58B7752-438F-43BD-9312-9D25E2521219")
+
+await page.locator("#twotabsearchtextbox").focus();
+await page.locator("#twotabsearchtextbox").fill("macbook");
+
+    page.locator('[role="button"]', { hasText: 'macbook m1' }).click();
+
+
+
+})
